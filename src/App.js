492651c537b37c59ba8bde30095b28e83acbf935
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-12-27 22:23:38 +0300

    settingstructure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Header from "../src/components/Header"
+import Nav from "../src/components/Nav"
+import Main from "../src/components/Main"
+import Footer from "../src/components/Footer"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>Homepage
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+    </>
   );
 }
 
